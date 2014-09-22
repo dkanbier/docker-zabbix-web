ebc9f18948dbbd8915719cb0de520773de72f8a3
Dennis Kanbier <dennis@kanbier.net>
2014-09-22 14:46:01 +0000

    Fixed typo in config

--- zabbix/zabbix.conf.php
+++ zabbix/zabbix.conf.php
@@ -3,16 +3,16 @@
 global $DB;
 
 $DB["TYPE"]                     = 'MYSQL';
-$DB["SERVER"]                   = getenv('ZABBIX-DB_PORT_3306_TCP_ADDR');
-$DB["PORT"]                     = getenv('ZABBIX-DB_PORT_3306_TCP_PORT');
+$DB["SERVER"]                   = getenv('ZABBIX_DB_PORT_3306_TCP_ADDR');
+$DB["PORT"]                     = getenv('ZABBIX_DB_PORT_3306_TCP_PORT');
 $DB["DATABASE"]                 = 'zabbix';
 $DB["USER"]                     = 'zabbix';
 $DB["PASSWORD"]                 = 'zabbix';
 // SCHEMA is relevant only for IBM_DB2 database
 $DB["SCHEMA"]                   = '';
 
-$ZBX_SERVER                     = getenv('ZABBIX-SERVER_PORT_10051_TCP_ADDR');
-$ZBX_SERVER_PORT                = getenv('ZABBIX-SERVER_PORT_10051_TCP_PORT');
+$ZBX_SERVER                     = getenv('ZABBIX_SERVER_PORT_10051_TCP_ADDR');
+$ZBX_SERVER_PORT                = getenv('ZABBIX_SERVER_PORT_10051_TCP_PORT');
 $ZBX_SERVER_NAME                = '';
 
 $IMAGE_FORMAT_DEFAULT   = IMAGE_FORMAT_PNG;
